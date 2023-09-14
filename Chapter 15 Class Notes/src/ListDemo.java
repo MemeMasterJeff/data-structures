417b0e50c675975118548fb8cf8c46dc7c0eaa07
William Wang <65054034+MemeMasterJeff@users.noreply.github.com>
2023-09-14 13:27:47 -0500

    no

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -7,8 +7,28 @@ import java.util.ListIterator;
 */
 public class ListDemo
 {
-    public static void main(String[] args)
-    {
-        
+    public static void main(String[] args){
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+        //the list is currently:DHRT
+        /* the listIterator method creates a new list iterator
+        that is positioned at the head of the list
+        the "|" is used to represent the iterator position
+         */
+        ListIterator<String> iterator = staff.listIterator();// |DHRT
+
+        iterator.next();
+        // the next method also returns the element that the iterator is passing.
+        String name = iterator.next(); //DH|RT
+        System.out.println(name);
+
+        /* the add method for iterators inserts an element at the iterator postiion.
+        the iterator is then positioned after the element that was added.
+         */
+        iterator.add("Juliet");//DHJ|RT
+        iterator.add("Nina"); //DHJN|RT
     }
 }
