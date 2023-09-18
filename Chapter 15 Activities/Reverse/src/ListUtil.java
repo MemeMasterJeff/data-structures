76797712b45b5805445777f63ad4c62c305a8317
William Wang <65054034+MemeMasterJeff@users.noreply.github.com>
2023-09-18 12:59:51 -0500

    finished with reverser

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 
+
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
 */
@@ -12,6 +13,8 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        for(int i = 0;i<strings.size();i++){
+            strings.addFirst(strings.remove(i));
+        }
     }
 }
